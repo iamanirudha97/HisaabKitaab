fa9b52d3b269fa14cd0d9fb4b5009ca573108819
Anirudha Mahesh Dudhasagare <sagarlm10@gmail.com>
2024-03-03 22:47:39 -0500

    Delete Backend/routes/routes.js

--- Backend/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { registerUser } from "../controllers/userController.js";
-
-const userRouter = Router();
-//Create Users - First Name, Last Name, Username, email, bcrypt password
-userRouter.post("/register", registerUser);
-//login route
-
-export default userRouter;
